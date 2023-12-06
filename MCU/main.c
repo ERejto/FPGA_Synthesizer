2e50df427e79b72eb2e59898dc24107cbbbaebf9
Kevin Box <kbox@hmc.edu>
2023-12-06 00:00:57 -0800

    Update main with working spi from MCU to FPGA

--- MCU/main.c
+++ MCU/main.c
@@ -23,41 +23,41 @@ Purpose : Generic application start
 *   Application entry point.
 */
 int main(void) {
-  
+  gpioEnable(GPIO_PORT_A);
   RCC->CR &= 1; // MSI off
   RCC->CR &= ~(0xF<<4);
   RCC->CR |= RCC_CR_MSIRANGE_8; //increase clocking 16M?
   RCC->CR |= RCC_CR_MSIRGSEL;
   RCC->CR |= 1; // MSI ON
+  RCC->APB2ENR |= (RCC_APB2ENR_TIM15EN);
   initTIM(TIM15);
   RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOAEN | RCC_AHB2ENR_GPIOBEN | RCC_AHB2ENR_GPIOCEN);
-  DAC_init();
   char address = 0x40;
   uint8_t state = 0; // wait = 0; playing = 1;
-  char note = 0;
-
+  
 
   // "clock divide" = master clock frequency / desired baud rate
   // the phase for the SPI clock is 1 and the polarity is 0
   initSPI(1, 0, 0);
+/*
+  do {
+  uint32_t delay = 1;
+  uint16_t note = 0x0000;
+  delay_millis(TIM15, delay);
+  note = 0x0100;
+  playNote(note);
+  } while(1);
+  */
 
   do {
-  if (state == 0) {
-    char buttons = I2C_read(address);
-    if (buttons != 0 ) { 
-      note = encoder(buttons);
-      state = 1;
-    }
-  }
-  else if (state == 1) {
-    playNote(note);
-    if (I2C_read(address) == 0 ) { 
-      playNote(0);
-      state = 0;
-    }
-  } 
-}while (1);
+  uint32_t delay = 5000;
+  uint16_t note = 0x0000;
+  delay_millis(TIM15, delay);
+  delay_millis(TIM15, 100);
+  note = 0x0100;
+  playNote(note);
 
+}while (1);
 }
 
 
